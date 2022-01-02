714235afff506c7390d035e2169ed0775a2c2004
Sam Persis <sam.persis@outlook.com>
2022-01-02 11:38:25 +0100

    1. Modify Login Page Layout 2. Modify Register Page Layout 3. Fix Signout when closing window

--- BlogBravo/Startup.cs
+++ BlogBravo/Startup.cs
@@ -1,4 +1,5 @@
 using BlogBravo.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -36,6 +37,7 @@ namespace BlogBravo
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
 
             services.AddAsyncInitializer<DbInitializer>();
